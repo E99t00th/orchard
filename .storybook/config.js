49cd570b78daa07fa36fb7be023b4b4a5ce4f3d9
Dustin Gaudet <dustin.gaudet@Dustins-MacBook-Pro.local>
2019-04-22 21:06:26 -0400

    Config / setup for project

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,17 @@
-import { configure } from '@storybook/react';
+import { configure } from '@storybook/react'
+import '../src/index.css'
+import WebFont from 'webfontloader'
+
+WebFont.load({
+    google: {
+        families: ['Roboto:400,500', 'Material+Icons']
+    }
+})
+
+const req = require.context('../src', true, /.stories.js$/)
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
