303e2a1832e5a824d347a634430ccd6a6dcc4610
Dustin Gaudet <dustin.gaudet@Dustins-MacBook-Pro.local>
2019-04-25 08:33:46 -0400

    Update checkbox to accept onChange handler

--- src/components/anf-forms/Checkbox.js
+++ src/components/anf-forms/Checkbox.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Checkbox = ({name, checked, classes}) => {
+const Checkbox = ({name, checked, classes, onInputChange}) => {
     const htmlName = name.split(' ').join('-').toLowerCase()
 
     return (
         <>
             <label className={classes} htmlFor={htmlName}>
                 {name}
-                <input type="checkbox" className="margin-btm-small" checked={checked} name={htmlName} />
+                <input type="checkbox" className="margin-btm-small" checked={checked} name={htmlName} onChange={onInputChange} />
             </label>
         </>
     )
